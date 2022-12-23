1b1cbc27f307cd3961ffbf65b4a5d9b450ecaaac
Gerardo Guevara <gguevara@cikume.com>
2022-12-23 12:34:27 -0600

    updated test

--- servidores/servidores.go
+++ servidores/servidores.go
@@ -15,10 +15,12 @@ func GetServidores() {
 
 	// fmt.Println(reflect.TypeOf(inicio))
 	servidores := []string{
-		"http://platzi.com",
-		"http://google.com",
 		"http://facebook.com",
 		"http://instagram.com",
+		"http://twitter.com",
+		"http://google.com",
+		"http://amazon.com",
+		"http://microsoft.com",
 	}
 
 	for _, servidor := range servidores {
@@ -26,14 +28,14 @@ func GetServidores() {
 		fmt.Println(<-c)
 	}
 
-	fmt.Println("total: ", time.Since(inicio))
+	fmt.Println("finished in: ", time.Since(inicio))
 }
 
 // para verficar el tipo de un paquete de go solo se pone el paquete de Go y se busca el struct que debe empezar con mayuscula
 // para saber el tipo de una variable se puede utilizar t := reflect.TypeOf(x)
 // func revisarServidor(servidor string, t time.Time, c chan string) {..}
 
-func revisarServidor(servidor string, c chan string) {
+func revisarServidor(servidor string, c chan<- string) {
 	timePerRequest := time.Now()
 	_, err := http.Get(servidor)
 
